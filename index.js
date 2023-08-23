1e8c15f2fa25118c84237236d5ddff3f0a800c77
Gean109 <142437784+Gean109@users.noreply.github.com>
2023-08-23 17:20:50 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,111 @@
+/*
+
+let a = 3;
+
+let b = 5;
+let c = 6;
+let d = 7;
+let e = 4;
+
+console.log(a % 2);
+console.log(b % 2);
+console.log(c % 2);
+console.log(d % 2);
+console.log(e % 2);
+
+
+//minhaIdade
+
+let minhaIdade = 16
+console.log("Antes", minhaIdade);
+minhaIdade = minhaIdade + 1;
+console.log("Depois", minhaIdade);
+
+
+ 
+
+let A = 3;
+let B = 4;
+console.log(A + B);
+
+let C = 3;
+let D = 5;
+let E = 2;
+let F = -1;
+console.log(C * D / 2);
+console.log((D - B) * F);
+let G = 234;
+let H = 5;
+console.log(G % H );
+
+
+//Exercicio2
+
+
+let n1 = 3;
+let n2 = 4;
+console.log(n1 === n2);
+console.log(n1 !== n2);
+console.log(n1 > n2);
+console.log(n1 < n2);
+
+
+*/
+//comparadores logicos
+/*
+let A = 5;
+let B = 3;
+let C = 0;
+
+console.log(A === B || A !== C);
+console.log(A === B && A !== C);
+console.log("---- separador ----");
+
+console.log(true || false);
+console.log(true && false);
+console.log(true && true);
+
+//exercicio 3
+*/
+/*
+
+let a = true;
+let b = false;
+let c = true;
+
+console.log(a && b);
+console.log(b && c);
+console.log(a && c);
+console.log(a && b && c);
+
+*/
+/*
+let a = true;
+let b = false;
+let c = true;
+
+console.log(a || b);
+console.log(b || c);
+console.log(a || c);
+console.log(a || b || c);
+
+*/
+//negação
+/*
+
+let a = true;
+let b = false;
+
+console.log(b);
+console.log(!b);
+*/
+
+
+//pr
+
+let seuNome = "Gean";
+let idade = 16;
+console.log("nome:" , seuNome);
+console.log("idade:" , idade);
+console.log("É maior de idade?", idade >=18);
+console.log("Idade em 2050:", idade + 27);
